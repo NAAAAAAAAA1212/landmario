e5e169ca6cdb2f25a2dbe5c2d9dc226b82abd58d
NAAAAAAAAA1212 <96600575+NAAAAAAAAA1212@users.noreply.github.com>
2023-01-20 20:28:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
-scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖2`, function (sprite, location) {
-    game.over(true)
-})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0) {
         mySprite.vy = -175
     }
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖0`, function (sprite, location) {
-    tiles.placeOnRandomTile(mySprite, assets.tile`我的貼圖1`)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`終點`, function (sprite, location) {
+    game.over(true)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`死亡點`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`草方塊`)
 })
 let mySprite: Sprite = null
 let relive = 0
 mySprite = sprites.create(assets.image`角色`, SpriteKind.Player)
 scene.setBackgroundColor(9)
 scene.cameraFollowSprite(mySprite)
-tiles.setCurrentTilemap(tilemap`層級1`)
-tiles.placeOnRandomTile(mySprite, assets.tile`我的貼圖1`)
+tiles.setCurrentTilemap(tilemap`遊戲關卡`)
+tiles.placeOnRandomTile(mySprite, assets.tile`草方塊`)
 controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 350
