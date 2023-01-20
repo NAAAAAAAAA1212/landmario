f43bcbe6d7a199e4848ca973bae2bb98c06a72ff
NAAAAAAAAA1212 <96600575+NAAAAAAAAA1212@users.noreply.github.com>
2023-01-20 20:30:33 +0800

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`終點`, function (sprite, lo
     game.over(true)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`死亡點`, function (sprite, location) {
-    tiles.placeOnRandomTile(mySprite, assets.tile`草方塊`)
+    tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
 })
 let mySprite: Sprite = null
 let relive = 0
@@ -15,6 +15,6 @@ mySprite = sprites.create(assets.image`角色`, SpriteKind.Player)
 scene.setBackgroundColor(9)
 scene.cameraFollowSprite(mySprite)
 tiles.setCurrentTilemap(tilemap`遊戲關卡`)
-tiles.placeOnRandomTile(mySprite, assets.tile`草方塊`)
+tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
 controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 350
