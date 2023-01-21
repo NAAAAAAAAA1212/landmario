0f69c8a8cd5f9fb6fde45328d21be8205e1c93c8
NAAAAAAAAA1212 <96600575+NAAAAAAAAA1212@users.noreply.github.com>
2023-01-21 11:58:25 +0800

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖2`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0) {
         mySprite.vy = -175
@@ -6,11 +9,25 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`終點`, function (sprite, location) {
     game.over(true)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖4`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖3`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖1`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖0`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`死亡點`, function (sprite, location) {
     tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
 })
 let mySprite: Sprite = null
-let relive = 0
 mySprite = sprites.create(assets.image`角色`, SpriteKind.Player)
 scene.setBackgroundColor(9)
 scene.cameraFollowSprite(mySprite)
