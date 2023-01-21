fa7662c93b0d92973f5ef13fd1d1fe9cce0ee1e2
NAAAAAAAAA1212 <96600575+NAAAAAAAAA1212@users.noreply.github.com>
2023-01-21 18:56:36 +0800

    更新 main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
 scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖2`, function (sprite, location) {
     tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+    tiles.setTileAt(location, assets.tile`我的貼圖7`)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`終點0`, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖6`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
     tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -10,7 +15,8 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`終點`, function (sprite, location) {
-    game.over(true)
+    level += 1
+    nextlevel()
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖4`, function (sprite, location) {
     info.setLife(30000)
@@ -22,6 +28,15 @@ info.onLifeZero(function () {
     tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
     info.setLife(30)
 })
+function nextlevel () {
+    if (level == 1) {
+        tiles.setCurrentTilemap(tilemap`層級3`)
+        tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
+    }
+    if (level == 2) {
+        game.over(true)
+    }
+}
 scene.onOverlapTile(SpriteKind.Player, assets.tile`我的貼圖`, function (sprite, location) {
     tiles.placeOnRandomTile(mySprite, assets.tile`復活點1`)
 })
@@ -32,6 +47,8 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`復活點1`, function (sprite
     info.setLife(30000)
 })
 let mySprite: Sprite = null
+let level = 0
+level = 1
 info.setLife(30000)
 mySprite = sprites.create(assets.image`角色`, SpriteKind.Player)
 scene.setBackgroundColor(9)
